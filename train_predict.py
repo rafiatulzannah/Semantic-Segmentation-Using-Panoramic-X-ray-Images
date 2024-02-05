dcba4a664d7932934b54eee271b6b470146e000c
rafiatulzannah <90060965+rafiatulzannah@users.noreply.github.com>
2024-02-05 15:48:30 +0600

    train_predict.py

--- train_predict.py
+++ train_predict.py
@@ -15,8 +15,6 @@ from keras import metrics
 import tensorflow_addons as tfa
 
 
-project_path = 'C:\\Users\\18101552\\PycharmProjects\\thesis\\thesis_final'
-
 image_names = glob.glob(project_path + "\\dataset\\train_images\\train\\*.png")
 image_names.sort()
 images = [cv2.imread(img, 0) for img in image_names]
@@ -29,10 +27,7 @@ masks = [cv2.imread(mask, 0) for mask in mask_names]
 mask_dataset = np.array(masks)
 mask_dataset = np.expand_dims(mask_dataset, axis=3)
 
-image_dataset = image_dataset / 255.
-mask_dataset = mask_dataset / 255.
 
-# X_train, X_test, y_train, y_test = train_test_split(image_dataset, mask_dataset, test_size=0.20, random_state=42)
 X_train, X_temp, y_train, y_temp = train_test_split(image_dataset, mask_dataset, test_size=0.2, random_state=42)
 X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
 
@@ -52,11 +47,6 @@ def attention_unet(filters, output_channels, width=None, height=None, input_chan
 
         return d
 
-    def deconv2d(layer_input, filters):
-        u = Conv2DTranspose(filters, 2, strides=(2, 2), padding='same')(layer_input)
-        u = BatchNormalization()(u)
-        u = Activation('relu')(u)
-        return u
 
     def attention_block(F_g, F_l, F_int):
         g = Conv2D(F_int, kernel_size=(1, 1), strides=(1, 1), padding='valid')(F_g)
@@ -239,80 +229,7 @@ def dense_unet(filters, output_channels, width=None, height=None, input_channels
     return model
 
 
-def inception_unet(filters, output_channels, width=None, height=None, input_channels=1):
-    def InceptionModule(inputs, filters):
-        tower0 = Conv2D(filters, (1, 1), padding='same')(inputs)
-        tower0 = BatchNormalization()(tower0)
-        tower0 = Activation('relu')(tower0)
 
-        tower1 = Conv2D(filters, (1, 1), padding='same')(inputs)
-        tower1 = BatchNormalization()(tower1)
-        tower1 = Activation('relu')(tower1)
-        tower1 = Conv2D(filters, (3, 3), padding='same')(tower1)
-        tower1 = BatchNormalization()(tower1)
-        tower1 = Activation('relu')(tower1)
-
-        tower2 = Conv2D(filters, (1, 1), padding='same')(inputs)
-        tower2 = BatchNormalization()(tower2)
-        tower2 = Activation('relu')(tower2)
-        tower2 = Conv2D(filters, (3, 3), padding='same')(tower2)
-        tower2 = Conv2D(filters, (3, 3), padding='same')(tower2)
-        tower2 = BatchNormalization()(tower2)
-        tower2 = Activation('relu')(tower2)
-
-        tower3 = MaxPooling2D((3, 3), strides=(1, 1), padding='same')(inputs)
-        tower3 = Conv2D(filters, (1, 1), padding='same')(tower3)
-        tower3 = BatchNormalization()(tower3)
-        tower3 = Activation('relu')(tower3)
-
-        inception_module = concatenate([tower0, tower1, tower2, tower3], axis=3)
-
-        return inception_module
-
-    def deconv2d(layer_input, filters):
-        u = Conv2DTranspose(filters, 2, strides=(2, 2), padding='same')(layer_input)
-        u = BatchNormalization()(u)
-        u = Activation('relu')(u)
-
-        return u
-
-    inputs = Input(shape=(width, height, input_channels))
-
-    conv1 = InceptionModule(inputs, filters)
-    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
-
-    conv2 = InceptionModule(pool1, filters * 2)
-    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
-
-    conv3 = InceptionModule(pool2, filters * 4)
-    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
-
-    conv4 = InceptionModule(pool3, filters * 8)
-    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
-
-    conv5 = InceptionModule(pool4, filters * 16)
-
-    up6 = deconv2d(conv5, filters * 8)
-    up6 = InceptionModule(up6, filters * 8)
-    merge6 = concatenate([conv4, up6], axis=3)
-
-    up7 = deconv2d(merge6, filters * 4)
-    up7 = InceptionModule(up7, filters * 4)
-    merge7 = concatenate([conv3, up7], axis=3)
-
-    up8 = deconv2d(merge7, filters * 2)
-    up8 = InceptionModule(up8, filters * 2)
-    merge8 = concatenate([conv2, up8], axis=3)
-
-    up9 = deconv2d(merge8, filters)
-    up9 = InceptionModule(up9, filters)
-    merge9 = concatenate([conv1, up9], axis=3)
-
-    outputs = Conv2D(output_channels, kernel_size=(1, 1), strides=(1, 1), activation='sigmoid')(merge9)
-
-    model = Model(inputs=inputs, outputs=outputs)
-
-    return model
 
 
 def r2_unet(filters, output_channels, width=None, height=None, input_channels=1, conv_layers=2, rr_layers=2):
@@ -339,10 +256,6 @@ def r2_unet(filters, output_channels, width=None, height=None, input_channels=1,
 
         return d
 
-    def RRCNN_block(layer_input, filters, conv_layers=2, rr_layers=2):
-        d = Conv2D(filters, kernel_size=(3, 3), strides=(1, 1), padding='same')(layer_input)
-        d1 = recurrent_block(d, filters, conv_layers=conv_layers, rr_layers=rr_layers)
-        return Add()([d, d1])
 
     def deconv2d(layer_input, filters):
         u = Conv2DTranspose(filters, 2, strides=(2, 2), padding='same')(layer_input)
@@ -488,12 +401,6 @@ def se_unet(filters, output_channels, width=None, height=None, input_channels=1,
         x = multiply([init, se])
         return x
 
-    def sse_block(inp):
-        x = Conv2D(1, kernel_size=(1, 1), activation='sigmoid', use_bias=False)(inp)
-        x = multiply([inp, x])
-
-        return x
-
     def scse_block(inp, ratio=2):
         x1 = cse_block(inp, ratio)
         x2 = sse_block(inp)
@@ -550,98 +457,7 @@ def se_unet(filters, output_channels, width=None, height=None, input_channels=1,
     return model
 
 
-def unetpp(filters, output_channels, width=None, height=None, input_channels=1, conv_layers=2):
-    def conv2d(layer_input, filters, conv_layers=2):
-        d = Conv2D(filters, kernel_size=(3, 3), strides=(1, 1), padding='same')(layer_input)
-        d = BatchNormalization()(d)
-        d = Activation('relu')(d)
 
-        for i in range(conv_layers - 1):
-            d = Conv2D(filters, kernel_size=(3, 3), strides=(1, 1), padding='same')(d)
-            d = BatchNormalization()(d)
-            d = Activation('relu')(d)
-
-        return d
-
-    def deconv2d(layer_input, filters):
-        u = Conv2DTranspose(filters, 2, strides=(2, 2), padding='same')(layer_input)
-        u = BatchNormalization()(u)
-        u = Activation('relu')(u)
-        return u
-
-    inputs = Input(shape=(width, height, input_channels))
-
-    conv00 = conv2d(inputs, filters, conv_layers=conv_layers)
-    pool0 = MaxPooling2D((2, 2))(conv00)
-
-    conv10 = conv2d(pool0, filters * 2, conv_layers=conv_layers)
-    pool1 = MaxPooling2D((2, 2))(conv10)
-
-    conv01 = deconv2d(conv10, filters)
-    conv01 = concatenate([conv00, conv01])
-    conv01 = conv2d(conv01, filters, conv_layers=conv_layers)
-
-    conv20 = conv2d(pool1, filters * 4, conv_layers=conv_layers)
-    pool2 = MaxPooling2D((2, 2))(conv20)
-
-    conv11 = deconv2d(conv20, filters)
-    conv11 = concatenate([conv10, conv11])
-    conv11 = conv2d(conv11, filters, conv_layers=conv_layers)
-
-    conv02 = deconv2d(conv11, filters)
-    conv02 = concatenate([conv00, conv01, conv02])
-    conv02 = conv2d(conv02, filters, conv_layers=conv_layers)
-
-    conv30 = conv2d(pool2, filters * 8, conv_layers=conv_layers)
-    pool3 = MaxPooling2D((2, 2))(conv30)
-
-    conv21 = deconv2d(conv30, filters)
-    conv21 = concatenate([conv20, conv21])
-    conv21 = conv2d(conv21, filters, conv_layers=conv_layers)
-
-    conv12 = deconv2d(conv21, filters)
-    conv12 = concatenate([conv10, conv11, conv12])
-    conv12 = conv2d(conv12, filters, conv_layers=conv_layers)
-
-    conv03 = deconv2d(conv12, filters)
-    conv03 = concatenate([conv00, conv01, conv02, conv03])
-    conv03 = conv2d(conv03, filters, conv_layers=conv_layers)
-
-    conv40 = conv2d(pool3, filters * 16)
-
-    conv31 = deconv2d(conv40, filters * 8)
-    conv31 = concatenate([conv31, conv30])
-    conv31 = conv2d(conv31, 8 * filters, conv_layers=conv_layers)
-
-    conv22 = deconv2d(conv31, filters * 4)
-    conv22 = concatenate([conv22, conv20, conv21])
-    conv22 = conv2d(conv22, 4 * filters, conv_layers=conv_layers)
-
-    conv13 = deconv2d(conv22, filters * 2)
-    conv13 = concatenate([conv13, conv10, conv11, conv12])
-    conv13 = conv2d(conv13, 2 * filters, conv_layers=conv_layers)
-
-    conv04 = deconv2d(conv13, filters)
-    conv04 = concatenate([conv04, conv00, conv01, conv02, conv03], axis=3)
-    conv04 = conv2d(conv04, filters, conv_layers=conv_layers)
-
-    outputs = Conv2D(output_channels, kernel_size=(1, 1), strides=(1, 1), activation='sigmoid')(conv04)
-
-    model = Model(inputs=inputs, outputs=outputs)
-
-    return model
-
-
-def dice_coef(y_true, y_pred, smooth=100):
-    y_true_f = K.flatten(y_true)
-    y_pred_f = K.flatten(y_pred)
-    intersection = K.sum(y_true_f * y_pred_f)
-    dice = (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
-    return dice
-
-
-def dice_coef_loss(y_true, y_pred):
-    return 1 - dice_coef(y_true, y_pred)
 
 
 def create_model(model_name, filters, conv_layers, rr_layers):
@@ -693,7 +509,6 @@ def train_predict(model_name, filters=16, conv_layers=2, rr_layers=None, learnin
 
     history = model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(X_test, y_test), shuffle=False)
 
-    model.save(results_path + '\\trained_model.hdf5')
 
     print("\n\nModel:", model_name)
     print("Parameters:", model.count_params())
@@ -760,7 +575,7 @@ def train_predict(model_name, filters=16, conv_layers=2, rr_layers=None, learnin
     plt.savefig(results_path + '\\training_validation_iou.png')
     plt.show()
 
-    """# **Predicting on a Test Patch**"""
+    """# **Prediction**"""
 
     test_img_number = random.randint(0, len(X_test)-1)
     test_img = X_test[test_img_number]
@@ -781,54 +596,6 @@ def train_predict(model_name, filters=16, conv_layers=2, rr_layers=None, learnin
     plt.imshow(prediction)
     plt.show()
 
-    """# **Predicting on a Test Image**"""
-    model = load_model(results_path + '\\trained_model.hdf5', compile=False)
-
-    large_image = cv2.imread(project_path + '\\large_image\\large.png', 0)
-    patches = patchify(large_image, (256, 256), step=256)
-
-    predicted_patches = []
-
-    for i in range(patches.shape[0]):
-        for j in range(patches.shape[1]):
-            single_patch = patches[i, j, :, :]
-            single_patch = single_patch / 255.
-            single_patch_input = np.expand_dims(single_patch, 0)
-
-            single_patch_prediction = (model.predict(single_patch_input)[0, :, :, 0] > threshold).astype(np.uint8)
-            predicted_patches.append(single_patch_prediction)
-
-    predicted_patches = np.array(predicted_patches)
-    predicted_patches_reshaped = np.reshape(predicted_patches, (patches.shape[0], patches.shape[1], 256, 256))
-
-    plt.figure(figsize=(12, 12))
-    square = 4
-    ix = 1
-    for i in range(square):
-        for j in range(square):
-            ax = plt.subplot(square, square, ix)
-            ax.set_xticks([])
-            ax.set_yticks([])
-            plt.imshow(predicted_patches_reshaped[i, j, :, :])
-            ix += 1
-    plt.savefig(results_path + '\\prediction_patches.png')
-    plt.show()
-
-    reconstructed_image = unpatchify(predicted_patches_reshaped, large_image.shape)
-    plt.imsave(
-        results_path + '\\prediction_reconstructed.png',
-        reconstructed_image
-    )
-
-    plt.figure(figsize=(28, 14))
-    plt.subplot(221)
-    plt.title('Image')
-    plt.imshow(large_image, cmap='gray')
-    plt.subplot(222)
-    plt.title('Prediction')
-    plt.imshow(reconstructed_image)
-    plt.savefig(results_path + '\\result_(image_and_prediction).png')
-    plt.show()
 
 
 train_predict(model_name='residual_unet', filters=16, conv_layers=3, learning_rate=0.0001, batch_size=32, epochs=100, threshold=0.75)
